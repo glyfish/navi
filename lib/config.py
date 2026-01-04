bc043ae2b063193bbe13a6a70d84452332803861
Troy Stribling <troy.stribling@gmail.com>
2026-01-04 12:59:53 -0600

    fixed type warnings

--- lib/config.py
+++ lib/config.py
@@ -2,6 +2,9 @@ import os
 import pathlib
 import matplotlib
 
+from typing import Literal, Sequence
+from matplotlib import colors as mcolors
+
 from IPython.display import Image
 from cycler import cycler
 
@@ -26,32 +29,40 @@ glyfish_style = style_file
 
 plot_asset_path = os.path.join(os.getcwd(), 'plots')
 
-color = matplotlib.colors.ColorConverter().to_rgb
+color = mcolors.ColorConverter().to_rgb
 
-histogram_color_map_cdict = {'red':   ((0.0,  1.0, 1.0),
-                                       (0.5,  0.19, 0.19),
-                                       (0.75, 1.0, 1.0),
-                                       (1.0,  1.0, 1.0)),
+ColorKey = Literal["red", "green", "blue", "alpha"]
+ColorStops = Sequence[tuple[float, float, float]]
 
-                             'green': ((0.0,  1.0, 1.0),
-                                       (0.5, 0.62, 0.62),
-                                       (0.75, 0.58, 0.58),
-                                       (1.0,  0.91, 0.91)),
-
-                             'blue':  ((0.0,  1.0, 1.0),
-                                       (0.5,  1.0, 1.0),
-                                       (0.75, 0.0, 0.0),
-                                       (1.0,  0.0, 0.0))
-                             }
-histogram_color_map = matplotlib.colors.LinearSegmentedColormap('HistogramMap', histogram_color_map_cdict)
+histogram_color_map_cdict: dict[ColorKey, ColorStops] = {
+    "red": (
+        (0.0, 1.0, 1.0),
+        (0.5, 0.19, 0.19),
+        (0.75, 1.0, 1.0),
+        (1.0, 1.0, 1.0),
+    ),
+    "green": (
+        (0.0, 1.0, 1.0),
+        (0.5, 0.62, 0.62),
+        (0.75, 0.58, 0.58),
+        (1.0, 0.91, 0.91),
+    ),
+    "blue": (
+        (0.0, 1.0, 1.0),
+        (0.5, 1.0, 1.0),
+        (0.75, 0.0, 0.0),
+        (1.0, 0.0, 0.0),
+    ),
+}
+histogram_color_map = mcolors.LinearSegmentedColormap('HistogramMap', histogram_color_map_cdict)
 
 alternate_color_map_colors = [color('white'), color("#8C35FF"), color("#0067C4"), color("#329EFF"), color("#FF9500"), color("#FFE800")]
-alternate_color_map =  matplotlib.colors.LinearSegmentedColormap.from_list('AlternateMap', alternate_color_map_colors, N=100 )
+alternate_color_map =  mcolors.LinearSegmentedColormap.from_list('AlternateMap', alternate_color_map_colors, N=100 )
 
-contour_color_map = matplotlib.colors.ListedColormap(["#0067C4", "#FFE800", "#320075", "#FF9500",
+contour_color_map = mcolors.ListedColormap(["#0067C4", "#FFE800", "#320075", "#FF9500",
                                                       "#329EFF", "#AC9C00", "#5600C9", "#FFC574",
                                                       "#003B6F", "#FFEB22", "#8C35FF", "#AC6500"])
-alternate_contour_color_map = matplotlib.colors.ListedColormap(["#003B6F", "#FFEB22", "#FFC574", "#320075"])
+alternate_contour_color_map = mcolors.ListedColormap(["#003B6F", "#FFEB22", "#FFC574", "#320075"])
 
 distribution_sample_cycler = cycler("color", ["#329EFF", "#320075"])
 alternate_cycler = cycler("color", ["#0067C4", "#8C35FF", "#FF9500", "#FFE800", "#329EFF", "#FFC574", "#320075"])
